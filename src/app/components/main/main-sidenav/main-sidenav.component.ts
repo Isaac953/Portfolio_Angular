2d8e031e9986d5b9e6608b396b2d7f257bd111fe
isaac953 <isaac.arias07@gmail.com>
2022-06-24 15:58:49 -0600

    Basic navbar completed

--- src/app/components/main/main-sidenav/main-sidenav.component.ts
+++ src/app/components/main/main-sidenav/main-sidenav.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faUser,
+  faCode,
+  faPencilAlt,
+  faBriefcase,
+  faCommentAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: '.app-main-sidenav',
   templateUrl: './main-sidenav.component.html',
-  styleUrls: ['./main-sidenav.component.css']
+  styleUrls: ['./main-sidenav.component.css'],
 })
 export class MainSidenavComponent implements OnInit {
+  faUser = faUser;
+  faCode = faCode;
+  faPencilAlt = faPencilAlt;
+  faBriefcase = faBriefcase;
+  faCommentAlt = faCommentAlt;
 
-  constructor( ) { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
