1bf8d1a2605e33c5af583ffc00e27eba4da0068e
isaac953 <isaac.arias07@gmail.com>
2022-07-26 20:46:56 -0600

    Agregando Opciones de Sidenav

--- src/app/components/main/sidenav/sidenav.component.ts
+++ src/app/components/main/sidenav/sidenav.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faUser,
+  faCode,
+  faPencilAlt,
+  faBriefcase,
+  faCommentAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
 })
 export class SidenavComponent implements OnInit {
+  faUser = faUser;
+  faCode = faCode;
+  faPencilAlt = faPencilAlt;
+  faBriefcase = faBriefcase;
+  faCommentAlt = faCommentAlt;
 
-  constructor() { }
+  navComponents: any[] = [
+    {
+      component: 'Sobre mí',
+      route: '/about-me',
+      icon: faUser,
+    },
+    {
+      component: 'Servicios',
+      route: '/services',
+      icon: faCode,
+    },
+    {
+      component: 'Habilidades',
+      route: '/skills',
+      icon: faPencilAlt,
+    },
+    {
+      component: 'Proyectos',
+      route: '/proyects',
+      icon: faBriefcase,
+    },
+    {
+      component: 'Contacto',
+      route: '/contact',
+      icon: faCommentAlt,
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
