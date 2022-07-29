d5482593d3ac2faf492eca4c06180a672e18bac5
isaac953 <isaac.arias07@gmail.com>
2022-07-29 15:35:39 -0600

    Rework parte 5: Implementación básica de routing de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutmeComponent } from './components/main/mcontent/aboutme/aboutme.component';
+import { ContactComponent } from './components/main/mcontent/contact/contact.component';
+import { HomeComponent } from './components/main/mcontent/home/home.component';
+import { MyservicesComponent } from './components/main/mcontent/myservices/myservices.component';
+import { ProyectsComponent } from './components/main/mcontent/proyects/proyects.component';
+import { SkillsComponent } from './components/main/mcontent/skills/skills.component';
 
-// const routes: Routes = [
-//   { path: 'home', component: HomeComponent },
-//   { path: 'about-me', component: AboutmeComponent },
-//   { path: 'services', component: MyservicesComponent },
-//   { path: 'skills', component: SkillsComponent },
-//   { path: 'proyects', component: ProyectsComponent },
-//   { path: 'contact', component: ContactComponent },
-//   { path: '', redirectTo: 'home', pathMatch: 'full' },
-// ];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about-me', component: AboutmeComponent },
+  { path: 'services', component: MyservicesComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'proyects', component: ProyectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
+export const routingComponents = [HomeComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './components/header/header.component';
@@ -18,6 +18,7 @@ import { McontentComponent } from './components/main/mcontent/mcontent.component
     FooterComponent,
     SidenavComponent,
     McontentComponent,
+    routingComponents,
   ],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
