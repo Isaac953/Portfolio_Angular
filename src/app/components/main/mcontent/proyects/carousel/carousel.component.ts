ab1427645d9ea11fd1c95f8a66df8214a64a3013
isaac953 <isaac.arias07@gmail.com>
2022-09-30 15:50:55 -0600

    Proyects Component Jornada 9: Configurar botones de adelante y atras por medio de funciones para capturar valor actual de variable

--- src/app/components/main/mcontent/proyects/carousel/carousel.component.ts
+++ src/app/components/main/mcontent/proyects/carousel/carousel.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-carousel',
+  selector: 'carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
+  marginLeft: any;
+  faArrowRight = faArrowRight;
+  faArrowLeft = faArrowLeft;
+  clickSum = 1;
 
-  constructor() { }
+  carouselSlides: any[] = [
+    {
+      carouselImg: 'Imagen 1',
+      carouselBackground: 'darkblue',
+      carouselClass: 'inactive',
+    },
+    {
+      carouselImg: 'Imagen 2',
+      carouselBackground: 'rgb(138, 138, 16)',
+      carouselClass: 'inactive',
+    },
+    {
+      carouselImg: 'Imagen 3',
+      carouselBackground: 'brown',
+      carouselClass: 'inactive',
+    },
+    {
+      carouselImg: 'Imagen 4',
+      carouselBackground: 'green',
+      carouselClass: 'inactive',
+    },
+    {
+      carouselImg: 'Imagen 5',
+      carouselBackground: 'red',
+      carouselClass: 'inactive',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  backSlide = (backValue: number) => {
+    this.clickSum = this.clickSum - backValue;
+    console.log(this.clickSum);
+    return backValue;
+  };
+
+  nextSlide = (nextValue: number) => {
+    this.clickSum = this.clickSum + nextValue;
+    console.log(this.clickSum);
+    return nextValue;
+  };
+
+  ngOnInit(): void {}
 }
