398ba0811d65eef90ab69301f512ab1d5a769491
isaac953 <isaac.arias07@gmail.com>
2022-10-01 00:55:52 -0600

    Parche a commit de Jornada 9 por ahora con NgStyle los botones

--- src/app/components/main/mcontent/proyects/carousel/carousel.component.ts
+++ src/app/components/main/mcontent/proyects/carousel/carousel.component.ts
@@ -11,32 +11,34 @@ export class CarouselComponent implements OnInit {
   faArrowRight = faArrowRight;
   faArrowLeft = faArrowLeft;
   clickSum = 1;
+  classSlideNext = 'slidenext';
+  classSlideBack = 'slideback';
 
   carouselSlides: any[] = [
     {
       carouselImg: 'Imagen 1',
       carouselBackground: 'darkblue',
-      carouselClass: 'inactive',
+      carouselMargin: 0,
     },
     {
       carouselImg: 'Imagen 2',
       carouselBackground: 'rgb(138, 138, 16)',
-      carouselClass: 'inactive',
+      carouselMargin: 0,
     },
     {
       carouselImg: 'Imagen 3',
       carouselBackground: 'brown',
-      carouselClass: 'inactive',
+      carouselMargin: 0,
     },
     {
       carouselImg: 'Imagen 4',
       carouselBackground: 'green',
-      carouselClass: 'inactive',
+      carouselMargin: 0,
     },
     {
       carouselImg: 'Imagen 5',
       carouselBackground: 'red',
-      carouselClass: 'inactive',
+      carouselMargin: 0,
     },
   ];
 
@@ -45,13 +47,57 @@ export class CarouselComponent implements OnInit {
   backSlide = (backValue: number) => {
     this.clickSum = this.clickSum - backValue;
     console.log(this.clickSum);
-    return backValue;
+    this.transitionSlideBack();
   };
 
   nextSlide = (nextValue: number) => {
     this.clickSum = this.clickSum + nextValue;
     console.log(this.clickSum);
-    return nextValue;
+    this.transitionSlideNext();
+  };
+
+  transitionSlideNext = () => {
+    switch (true) {
+      case this.clickSum == 1:
+        this.carouselSlides[0].carouselMargin = -700;
+        break;
+      case this.clickSum == 2:
+        this.carouselSlides[1].carouselMargin = -700;
+        break;
+      case this.clickSum == 3:
+        this.carouselSlides[2].carouselMargin = -700;
+        break;
+      case this.clickSum == 4:
+        this.carouselSlides[3].carouselMargin = -700;
+        break;
+      case this.clickSum == 5:
+        this.carouselSlides[4].carouselMargin = -700;
+        break;
+      default:
+        break;
+    }
+  };
+
+  transitionSlideBack = () => {
+    switch (true) {
+      case this.clickSum == 1:
+        this.carouselSlides[1].carouselMargin = 0;
+        break;
+      case this.clickSum == 2:
+        this.carouselSlides[2].carouselMargin = 0;
+        break;
+      case this.clickSum == 3:
+        this.carouselSlides[3].carouselMargin = 0;
+        break;
+      case this.clickSum == 4:
+        this.carouselSlides[4].carouselMargin = 0;
+        break;
+      case this.clickSum == 5:
+        this.carouselSlides[4].carouselMargin = 700;
+        break;
+      default:
+        break;
+    }
   };
 
   ngOnInit(): void {}
