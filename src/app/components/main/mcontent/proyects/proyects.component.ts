7b1a7dca6560ad14b7e3a087a0d0088c421458a5
isaac953 <isaac.arias07@gmail.com>
2022-10-25 13:41:57 -0600

    Ocultar proyectos no completados

--- src/app/components/main/mcontent/proyects/proyects.component.ts
+++ src/app/components/main/mcontent/proyects/proyects.component.ts
@@ -30,80 +30,80 @@ export class ProyectsComponent implements OnInit {
   proyectTitle: any;
 
   proyectsCards: any[] = [
-    {
-      coverProyect: './assets/images/platzi_video/slide_1.jpg',
-      titleProyect: 'Platzi Video',
-      year: '2021',
-      slides: [
-        {
-          proyect: './assets/images/platzi_video/slide_1.jpg',
-          caption: 'Dashboard principal',
-          dot: 1,
-          activeDot: 'active-dot',
-        },
-        {
-          proyect: './assets/images/platzi_video/slide_2.jpg',
-          caption: 'Inicio de sesión',
-          dot: 2,
-          activeDot: '',
-        },
-        {
-          proyect: './assets/images/platzi_video/slide_3.jpg',
-          caption: 'Error 404',
-          dot: 3,
-          activeDot: '',
-        },
-        {
-          proyect: './assets/images/platzi_video/slide_4.jpg',
-          caption: 'Registro de usuario',
-          dot: 4,
-          activeDot: '',
-        },
-        {
-          proyect: './assets/images/platzi_video/slide_5.jpg',
-          caption: 'Carousel de imagenes',
-          dot: 5,
-          activeDot: '',
-        },
-      ],
-      skills: [
-        { logo: faHtml5, name: 'HTML', class: 'html5' },
-        { logo: faCss3Alt, name: 'CSS', class: 'css3' },
-        { logo: faSass, name: 'Sass', class: 'sass' },
-        { logo: faGithub, name: 'Git/GitHub', class: 'git' },
-      ],
-      urlProyect: 'https://isaac953.github.io/Platzi_Video/',
-      descriptionProyect:
-        'Página web realizada con HTML5 y CSS3 en el curso de Frontend developer de Platzi con vistas de buscador de videos, carrusel de videos, login y registro de usuarios. Además con aprendizaje en el tema de maquetación web con Flexbox y CSS Grid.',
-    },
-    {
-      coverProyect: './assets/images/pagina_de_matematicas/slide_1.jpg',
-      titleProyect: 'Página de Matemáticas',
-      year: '2021',
-      slides: [
-        {
-          proyect: './assets/images/pagina_de_matematicas/slide_1.jpg',
-          caption: 'Dashboard principal de la Aplicación web',
-          dot: 1,
-          activeDot: 'active-dot',
-        },
-        {
-          proyect: './assets/images/pagina_de_matematicas/slide_2.jpg',
-          caption: 'Taller #1: Figuras geométricas',
-          dot: 2,
-          activeDot: '',
-        },
-      ],
-      skills: [
-        { logo: faHtml5, name: 'HTML', class: 'html5' },
-        { logo: faCss3Alt, name: 'CSS', class: 'css3' },
-        { logo: faJs, name: 'JavaScript', class: 'javascript' },
-        { logo: faGithub, name: 'Git/GitHub', class: 'git' },
-      ],
-      urlProyect: 'https://isaac953.github.io/platzi-curso-practico-javascript/',
-      descriptionProyect:
-        'Aplicación web realizada con HTML5, CSS3 y JavaScript en el curso práctico de JavaScript de Platzi con módulo de figuras geométricas; promedio, moda y mediana; y costos de salario implementando lo aprendido en los primeros dos módulos.',
-    },
+    // {
+    //   coverProyect: './assets/images/platzi_video/slide_1.jpg',
+    //   titleProyect: 'Platzi Video',
+    //   year: '2021',
+    //   slides: [
+    //     {
+    //       proyect: './assets/images/platzi_video/slide_1.jpg',
+    //       caption: 'Dashboard principal',
+    //       dot: 1,
+    //       activeDot: 'active-dot',
+    //     },
+    //     {
+    //       proyect: './assets/images/platzi_video/slide_2.jpg',
+    //       caption: 'Inicio de sesión',
+    //       dot: 2,
+    //       activeDot: '',
+    //     },
+    //     {
+    //       proyect: './assets/images/platzi_video/slide_3.jpg',
+    //       caption: 'Error 404',
+    //       dot: 3,
+    //       activeDot: '',
+    //     },
+    //     {
+    //       proyect: './assets/images/platzi_video/slide_4.jpg',
+    //       caption: 'Registro de usuario',
+    //       dot: 4,
+    //       activeDot: '',
+    //     },
+    //     {
+    //       proyect: './assets/images/platzi_video/slide_5.jpg',
+    //       caption: 'Carousel de imagenes',
+    //       dot: 5,
+    //       activeDot: '',
+    //     },
+    //   ],
+    //   skills: [
+    //     { logo: faHtml5, name: 'HTML', class: 'html5' },
+    //     { logo: faCss3Alt, name: 'CSS', class: 'css3' },
+    //     { logo: faSass, name: 'Sass', class: 'sass' },
+    //     { logo: faGithub, name: 'Git/GitHub', class: 'git' },
+    //   ],
+    //   urlProyect: 'https://isaac953.github.io/Platzi_Video/',
+    //   descriptionProyect:
+    //     'Página web realizada con HTML5 y CSS3 en el curso de Frontend developer de Platzi con vistas de buscador de videos, carrusel de videos, login y registro de usuarios. Además con aprendizaje en el tema de maquetación web con Flexbox y CSS Grid.',
+    // },
+    // {
+    //   coverProyect: './assets/images/pagina_de_matematicas/slide_1.jpg',
+    //   titleProyect: 'Página de Matemáticas',
+    //   year: '2021',
+    //   slides: [
+    //     {
+    //       proyect: './assets/images/pagina_de_matematicas/slide_1.jpg',
+    //       caption: 'Dashboard principal de la Aplicación web',
+    //       dot: 1,
+    //       activeDot: 'active-dot',
+    //     },
+    //     {
+    //       proyect: './assets/images/pagina_de_matematicas/slide_2.jpg',
+    //       caption: 'Taller #1: Figuras geométricas',
+    //       dot: 2,
+    //       activeDot: '',
+    //     },
+    //   ],
+    //   skills: [
+    //     { logo: faHtml5, name: 'HTML', class: 'html5' },
+    //     { logo: faCss3Alt, name: 'CSS', class: 'css3' },
+    //     { logo: faJs, name: 'JavaScript', class: 'javascript' },
+    //     { logo: faGithub, name: 'Git/GitHub', class: 'git' },
+    //   ],
+    //   urlProyect: 'https://isaac953.github.io/platzi-curso-practico-javascript/',
+    //   descriptionProyect:
+    //     'Aplicación web realizada con HTML5, CSS3 y JavaScript en el curso práctico de JavaScript de Platzi con módulo de figuras geométricas; promedio, moda y mediana; y costos de salario implementando lo aprendido en los primeros dos módulos.',
+    // },
     {
       coverProyect: './assets/images/portafolio_angular/slide_1.jpg',
       titleProyect: 'Portafolio Profesional',
